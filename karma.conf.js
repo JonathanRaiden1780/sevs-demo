f527ef433bfae72b489ed38d489bd936b1d5f13b
admin <admin@Jonathanmini.lan>
2021-02-05 13:51:53 -0600

    v1

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/CasanovaSevs'),
+      dir: require('path').join(__dirname, './coverage/DemoSevs'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
